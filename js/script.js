d0ea2dcfc6c2003c32907bb2f454f22faf52837f
zhouqunhui <458905964@qq.com>
2017-01-07 14:30:09 +0800

    finish
    finish

--- js/script.js
+++ js/script.js
@@ -6,21 +6,46 @@ window.onload = function () {
     var page1 = document.getElementById('page1');
     var page2 = document.getElementById('page2');
     var page3 = document.getElementById('page3');
+    var lantern = document.getElementsByClassName('p1_lantern')[0];
 
     var audio = document.getElementsByTagName("audio")[0];
 
+    audio.addEventListener("ended",function () {
+        pause();
+    }, false);
+
     music.addEventListener('touchstart',function () {
         if(audio.paused){
-            audio.play();
-            //this.setAttribute('class', 'play');
-            this.style.animationPlayState = "running";
-            this.style.webkitAnimationPlayState = "running";
+            play();
         }else{
-            audio.pause();
-            //this.setAttribute('class', '');
-            this.style.animationPlayState = "paused";
-            this.style.webkitAnimationPlayState = "paused";
+            pause();
         }
 
-    });
+    }, false);
+
+    function pause() {
+        audio.pause();
+        music.setAttribute('class', '');
+        // this.style.animationPlayState = "paused";
+        // this.style.webkitAnimationPlayState = "paused";
+    }
+
+    function play() {
+        audio.play();
+        music.setAttribute('class', 'play');
+        //this.style.animationPlayState = "running";
+        //this.style.webkitAnimationPlayState = "running";
+    }
+
+    lantern.addEventListener('touchstart',function () {
+        page1.style.display = 'none';
+        page2.style.display = 'block';
+        page3.style.display = 'block';
+        page3.style.top = "100%";
+        setTimeout(function () {
+            page2.setAttribute("class", "page fadeOut");
+            page3.setAttribute("class", "page fadeIn");
+        },5500);
+    }, false);
+
 };
